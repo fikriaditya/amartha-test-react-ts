a9a71b8c52b4922aa4bd3679a5cd4463f553be47
Fikriaditya <adityafikrighozali@gmail.com>
2025-12-19 17:09:22 +0700

    feat(main layout): adjust navigation, add logo

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,20 +1,24 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import logoUrl from '../assets/images/logo-amartha.png';
 
 const MainLayout = () => (
   <div className="app-container">
     <nav className="nav">
-      <NavLink to="/employees" className={({ isActive }) => isActive ? 'nav__link--active' : 'nav__link'}>
-        Employees
-      </NavLink>
-      <NavLink to="/wizard?role=admin" className="nav__link">
-        Add Employee (Admin)
-      </NavLink>
-      <NavLink to="/wizard?role=ops" className="nav__link">
-        Add Employee (Ops)
-      </NavLink>
-      <NavLink to="/style-guide" className="nav__link">
-        Style Guide
-      </NavLink>
+      <img src={logoUrl} alt="App Logo" className="nav__logo" />
+      <div className="nav__links">
+        <NavLink to="/employees" className={({ isActive }) => isActive ? 'nav__link--active' : 'nav__link'}>
+          Employees
+        </NavLink>
+        <NavLink to="/wizard?role=admin" className="nav__link">
+          Add Employee (Admin)
+        </NavLink>
+        <NavLink to="/wizard?role=ops" className="nav__link">
+          Add Employee (Ops)
+        </NavLink>
+        <NavLink to="/style-guide" className="nav__link">
+          Style Guide
+        </NavLink>
+      </div>
     </nav>
     <main className="content">
       <Outlet /> {/* This renders the child routes */}
