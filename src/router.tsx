6b2e5e5274392fbe10944eaac2257f465837011a
Fikriaditya <adityafikrighozali@gmail.com>
2025-12-19 14:26:19 +0700

    feat(wizard ui): add wizard pages and components, adjust routing

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 // import EmployeeListPage from './pages/EmployeeListPage';
-// import WizardPage from './pages/WizardPage';
+import WizardPage from './pages/WizardPage';
 import Styleguide from './pages/styleguide';
 import Home from './pages/home';
 
@@ -17,19 +17,19 @@ const router = createBrowserRouter([
       },
       {
         path: 'home',
-        element: <Home /> // The merged data list [cite: 52]
+        element: <Home />
       },
     //   {
     //     path: 'employees',
-    //     element: <EmployeeListPage /> // The merged data list [cite: 52]
-    //   },
-    //   {
-    //     path: 'wizard',
-    //     element: <WizardPage /> // Role-based form [cite: 5]
+    //     element: <EmployeeListPage /> // Employee List Page
     //   },
       {
+        path: 'wizard',
+        element: <WizardPage /> // Role-based Wizard form
+      },
+      {
         path: 'style-guide',
-        element: <Styleguide /> // Your custom CSS library
+        element: <Styleguide /> // STYLE GUIDE
       }
     ]
   }
